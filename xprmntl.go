9392deb3435aefd87dd792ee32b49649630e60e0
Josh Crowther <jshcrowthe@gmail.com>
2014-09-27 17:55:49 -0600

    Add error handling. Refactor to require more use of base datatypes

--- xprmntl.go
+++ xprmntl.go
@@ -2,15 +2,17 @@ package xprmntl
 
 import (
 	"fmt"
-	"os"
 	"net/http"
 //	"time"
 	"encoding/json"
 	"bytes"
 	"io/ioutil"
-//	"errors"
+	"errors"
+	"os"
 )
-
+/**
+ REQUEST OBJECTS
+ */
 /**
  STRUCT: Experiment
  */
@@ -20,211 +22,122 @@ type Experiment struct {
 	ExpDefault bool `json:"default"`;
 }
 
-func NewExperiment(name string) (Experiment) {
-	return Experiment{name, "", false};
-}
-
-func NewExperimentsList(experiments ...interface {}) ([]Experiment) {
-	list := []Experiment {};
-	for i := 0; i < len(experiments); i++ {
-		switch experiments[i].(type) {
-			case string: {
-				experiment := NewExperiment(experiments[i].(string));
-				list = append(list, experiment);
-			}
-			case Experiment: {
-				list = append(list, experiments[i].(Experiment));
-			}
-		}
-	}
-	return list;
-}
 /**
- END STRUCT
+ STRUCT: Config
  */
-
-/**
- STRUCT: SharedConfig
- */
-type SharedConfig struct {
-	DevKey string `json:"devKey"`;
+type Config struct {
+	DevKey      string `json:"devKey"`;
+	FeatureURL  string;
+	Timeout     int;
 	Experiments []Experiment `json:"experiments"`;
-}
-
-func NullSharedConfig() (SharedConfig) {
-	return SharedConfig{"", []Experiment{}};
-}
-
-func NewSharedConfig(devKey string) (SharedConfig) {
-	return SharedConfig{devKey, []Experiment{}};
-}
-/**
- END STRUCT
- */
-
-/**
- STRUCT: FeatureConfig
- */
-type FeatureConfig struct {
-	devKey string;
-	featureURL string;
-	timeout int;
-	experiments []Experiment `json:"experiments"`;
-	shared SharedConfig `json:"shared"`;
+	Shared      *Config `json:"shared"`;
 }
 
 /**
- FeatureConfig: Constructors
+ Config: GET functions
  */
-func NewFeatureConfig(experiments []Experiment) (FeatureConfig){
-	return FeatureConfig{os.Getenv("FEATURE_DEVKEY"), os.Getenv("FEATURE_URL"), 30000, experiments, NullSharedConfig()};
-}
-
-/**
- FeatureConfig: GET functions
- */
-func (c FeatureConfig) getDevKey() string {
-	return c.devKey;
+func (c Config) getDevKey() *string {
+	return &c.DevKey;
 };
 
-func (c FeatureConfig) getFeatureURL() string {
-	return c.featureURL;
+func (c Config) getFeatureURL() *string {
+	return &c.FeatureURL;
 };
 
-func (c FeatureConfig) getTimeout() int {
-	return c.timeout;
+func (c Config) getTimeout() int {
+	return c.Timeout;
 };
 
-func (c FeatureConfig) getExperiments() []Experiment {
-	return c.experiments;
+func (c Config) getExperiments() *[]Experiment {
+	return &c.Experiments;
 };
 
-func (c FeatureConfig) getSharedConfig() SharedConfig {
-	return c.shared;
+func (c Config) getSharedConfig() *Config {
+	return c.Shared;
 };
-
-/**
- END STRUCT
- */
 
 /**
  STRUCT: FeatureClient
  */
 type FeatureClient struct {
-	DevKey string `json:"devKey"`;
-	FeatureURL string `json:"featureUrl"`;
-	Timeout int `json:"timeout"`;
-	Experiments []Experiment `json:"experiments"`;
-	Shared SharedConfig `json:"shared"`;
+	DevKey      *string       `json:"devKey"`;
+	FeatureURL  *string       `json:"featureUrl"`;
+	Timeout     int           `json:"timeout"`;
+	Experiments *[]Experiment `json:"experiments"`;
+	Shared      *Config `json:"shared"`;
 }
 
-func experimentsListHandler(rawExperiments []interface {}) []Experiment {
-	var experiments []Experiment;
-
-	for i := 0; i < len(rawExperiments); i++ {
-		switch rawExperiments[i].(type) {
-		case string: {
-			experiments = append(experiments, NewExperiment(rawExperiments[i].(string)));
-		}
-		case map[string]interface {}: {
-			if rawExperiments[i].(map[string]interface{})["name"] == nil { fmt.Println("ERORR"); }
-			var description string;
-			var expDefault bool;
-			if rawExperiments[i].(map[string]interface{})["description"] != nil {
-				description = rawExperiments[i].(map[string]interface{})["description"].(string);
-			} else {
-				description = "";
-			}
-			if rawExperiments[i].(map[string]interface{})["default"] != nil {
-				expDefault = rawExperiments[i].(map[string]interface{})["default"].(bool);
-			}
-			experiments = append(experiments, Experiment{rawExperiments[i].(map[string]interface{})["name"].(string), description, expDefault});
-		}
-		}
-	}
-
-	return experiments;
-}
 /**
  FeatureClient: Constructors
  */
-func New(config map[string]interface {}) (FeatureClient) {
-	var devKey, featureURL string;
-	var timeout int;
+func New(config Config) (*FeatureClient, error) {
+	devKey      := config.getDevKey();
+	featureURL  := config.getFeatureURL();
+	timeout     := config.getTimeout();
+	experiments := config.getExperiments();
+	shared      := config.getSharedConfig();
 
-	rawDevKey       := config["devKey"]
-	rawFeatureURL   := config["featureUrl"]
-//	rawTimeout      := config["timeout"].(int);
-	rawExperiments  := config["experiments"].([]interface {});
-	rawSharedConfig := config["shared"];
-
-	if rawDevKey == nil {
-		devKey = os.Getenv("FEATURE_DEVKEY");
-	} else {
-		devKey = config["devKey"].(string);
-	}
-	if rawFeatureURL == nil {
-		featureURL = os.Getenv("FEATURE_URL")
-	} else {
-		featureURL = config["featureUrl"].(string);
+	if devKey == nil || len(*devKey) == 0 {
+		envKey := os.Getenv("FEATURE_DEVKEY");
+		if len(envKey) == 0 {
+			return nil, errors.New("XPRMNTL: New(): XPRMNTL requires a devKey to be set");
+		}
+		devKey = &envKey;
 	}
 
-	// Handle array of varried experiments
-	var experiments []Experiment;
-	if rawExperiments != nil {
-		experiments = experimentsListHandler(rawExperiments);
+	if featureURL == nil || len(*featureURL) == 0 {
+		envUrl := os.Getenv("FEATURE_URL");
+		if len(envUrl) == 0 {
+			return nil, errors.New("XPRMNTL: New(): XPRMNTL requires a featureUrl to be set");
+		}
+		featureURL = &envUrl;
 	}
 
-	// Handle shared map
-	var sharedConfig SharedConfig;
-	if rawSharedConfig != nil {
-		if (rawSharedConfig.(map[string]interface {})["devKey"] == nil ||
-			 rawSharedConfig.(map[string]interface {})["experiments"] == nil ||
-			 len(rawSharedConfig.(map[string]interface {})["experiments"].([]interface {})) == 0 ) {
-			 fmt.Println("THERE WAS AN ERROR");
-			 }
-		sharedConfig = SharedConfig{ DevKey      : rawSharedConfig.(map[string]interface {})["devKey"].(string),
-																 Experiments : rawSharedConfig.(map[string]interface {})["experiments"].([]Experiment)};
-	}
-
-	return FeatureClient{devKey, featureURL, timeout, experiments, sharedConfig};
+	return &FeatureClient{devKey, featureURL, timeout, experiments, shared}, nil;
 }
 
 /**
  FeatureClient: Utility
  */
-func (c FeatureClient) Announce() App {
-	url := c.FeatureURL + "api/coupling/";
+func (c FeatureClient) Announce() (*AppConfig, error) {
+	url := *c.FeatureURL + "/api/coupling/";
 //	var timeout time.Duration = time.Duration(c.Timeout) * time.Millisecond;
-	jsonBody, err := json.Marshal(c);
-	if err != nil {
-		fmt.Println("There was an Error");
+	jsonBody, jsonErr := json.Marshal(c);
+	if jsonErr != nil {
+		return nil, errors.New("XPRMNTL: Announce(): There was an error building your JSON")
+	} else {
+		fmt.Println(string(jsonBody[:]))
 	}
 	client := &http.Client{}
 
 	req, reqErr := http.NewRequest("POST", url, bytes.NewReader(jsonBody));
-	req.Header.Add("x-feature-key", c.DevKey);
+	req.Header.Add("x-feature-key", *c.DevKey);
 	req.Header.Add("Content-Type", "application/json");
 	if reqErr != nil {
-		fmt.Println("There was an Error");
+		return nil, errors.New("XPRMNTL: Announce(): There was an error in your request")
 	}
 	res, resErr := client.Do(req);
-	defer res.Body.Close();
-	body, bodyReadErr := ioutil.ReadAll(res.Body);
 	if resErr != nil {
-		fmt.Println("There was an Error");
+		fmt.Println(resErr);
+		return nil, errors.New("XPRMNTL: Announce(): There was an error in the server response")
 	}
+	
+	if (res.StatusCode != 200) {
+		fmt.Println(res);
+		return nil, errors.New("XPRMNTL: Announce(): Server return a non-200 response");
+	}
+	body, bodyReadErr := ioutil.ReadAll(res.Body);
+	defer res.Body.Close();
 	if bodyReadErr != nil {
-		fmt.Println("There was an Error");
+		return nil, errors.New("XPRMNTL: Announce(): There was an error in reading the server response body")
 	}
 
 	var response FeatureClientResponse;
-//	fmt.Println(string(body[:]))
 	marshalErr := json.Unmarshal(body, &response);
 	if marshalErr != nil {
-		fmt.Println("json.Unmarshal ERROR");
+		return nil, errors.New("XPRMNTL: (function) Announce: There was an error in parsing the JSON response")
 	}
-	return response.App;
+	return &response.App, nil;
 }
 /**
  END STRUCT
@@ -238,15 +151,14 @@ func (c FeatureClient) Announce() App {
  STRUCT: FeatureClientResponse
  */
 type FeatureClientResponse struct {
-	App App
+	App AppConfig
 }
 
-type App struct {
+/**
+ STRUCT: AppConfig
+ */
+type AppConfig struct {
 	Groups interface {};
 	Experiments map[string]interface {};
 	Envs interface {};
 }
-
-/**
- END STRUCT
- */
